139e695d861adb60632ad7e6cf4c41957218fa42
8088470191 <160418801+8088470191@users.noreply.github.com>
2024-10-23 22:49:48 +0530

    creating schema

---
+++ Country/src/India.java
@@ -0,0 +1,35 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class India {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String url="jdbc:mysql://localhost:3306/college";
+		String userName="root";
+		String password="Xworkzodc@123";
+		
+		
+		Connection connection=null;
+		try
+		{
+			connection=DriverManager.getConnection(url,userName,password);
+			if(connection!=null)
+			{
+				System.out.println("this is connected");
+			}
+			else
+			{
+				System.out.println("this is not  connected");
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
